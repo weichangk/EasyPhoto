a430b12a503d7a38c665219cc74b1e712c301fa5
weichangk <18276743761@163.com>
2024-07-09 08:03:59 +0800

    feat(imagekit):新增models

--- imagekit/inc/models.h
+++ imagekit/inc/models.h
@@ -8,6 +8,7 @@
 #pragma once
 #include <QPixmap>
 #include <QString>
+#include <QMap>
 
 // 暂不持支：svg ico cur xpm heic jfif heif jif rgf xwd jfi picon
 #define CONV_OUT_FORMATS "jpg jpeg webp hdr bmp gif dds psd tiff tga rgb avif pgm jp2 rgba ppm xbm pcx wbmp exr map jbg pnm jpe yuv pbm pdb g4 ras g3 pal sgi pict pfm pcd jps uyvy pgx vips six fts fax jbig ipl sun pam viff mng mtv xv pct sixel palm rgbo hrz otb"
@@ -32,49 +33,102 @@ using ImageFunc = enum {
 
 QString ImageFuncEnum2QString(ImageFunc param);
 
-using OutQuality = enum {
+using ImageQuality = enum {
     Low = 0,
     Medium,
     High,
 };
 
-using OutResolution = enum {
-// 标清分辨率（SD，Standard Definition）
+using ImageResolution = enum {
+    // 标清分辨率（SD，Standard Definition）
+    VGA = 0, // 640×480 (VGA)
+    NTSC,    // 720×480 (NTSC)
+    PAL,     // 720×576 (PAL)
 
-// 640×480 (VGA)
-// 720×480 (NTSC)
-// 720×576 (PAL)
-// 高清分辨率（HD，High Definition）
+    // 高清分辨率（HD，High Definition）
+    _720p, // 1280×720 (720p)
+    WXGAHD,  // 1366×768 (WXGA)
 
-// 1280×720 (720p)
-// 1366×768 (WXGA)
-// 全高清分辨率（Full HD）
+    // 全高清分辨率（Full HD）
+    _1080p, // 1920×1080 (1080p)
 
-// 1920×1080 (1080p)
-// 超高清分辨率（UHD，Ultra High Definition）
+    // 超高清分辨率（UHD，Ultra High Definition）
+    _2K, // 2560×1440 (1440p，2K)
+    _4K, // 3840×2160 (2160p，4K)
+    _5K, // 5120×2880 (5K)
+    _8K, // 7680×4320 (4320p，8K)
 
-// 2560×1440 (1440p，2K)
-// 3840×2160 (2160p，4K)
-// 5120×2880 (5K)
-// 7680×4320 (4320p，8K)
-// 宽屏分辨率
+    // 宽屏分辨率
+    WXGA,     // 1280×800 (WXGA)
+    WXGAPRO,  // 1440×900 (WXGA+)
+    WSXGAPRO, // 1680×1050 (WSXGA+)
+    WUXGA,    // 1920×1200 (WUXGA)
+    UWHD,     // 2560×1080 (UWHD)
+    UWQHD,    // 3440×1440 (UWQHD)
+    WQHDPRO,  // 3840×1600 (WQHD+)
 
-// 1280×800 (WXGA)
-// 1440×900 (WXGA+)
-// 1680×1050 (WSXGA+)
-// 1920×1200 (WUXGA)
-// 2560×1080 (UWHD)
-// 3440×1440 (UWQHD)
-// 3840×1600 (WQHD+)
-// 移动设备常见分辨率
+    // 移动设备常见分辨率
+    iPhone5,         // 1136×640 (iPhone 5/5S/5C, iPhone SE)
+    iPhone6,         // 1334×750 (iPhone 6/6S/7/8)
+    iPhone6Plus,     // 1920×1080 (iPhone 6 Plus/6S Plus/7 Plus/8 Plus)
+    iPhoneX,         // 2436×1125 (iPhone X/XS/11 Pro)
+    iPhoneXS,        // 2688×1242 (iPhone XS Max/11 Pro Max)
+    iPhone121314,    // 1170×2532 (iPhone 12/13/14)
+    iPhone121314Pro, // 1284×2778 (iPhone 12/13/14 Pro Max)
+};
 
-// 1136×640 (iPhone 5/5S/5C, iPhone SE)
-// 1334×750 (iPhone 6/6S/7/8)
-// 1920×1080 (iPhone 6 Plus/6S Plus/7 Plus/8 Plus)
-// 2436×1125 (iPhone X/XS/11 Pro)
-// 2688×1242 (iPhone XS Max/11 Pro Max)
-// 1170×2532 (iPhone 12/13/14)
-// 1284×2778 (iPhone 12/13/14 Pro Max)
+static const QMap<ImageResolution, QString> ImageResolutionDesMap = {
+    {VGA, "640×480"},
+    {NTSC, "720×480"},
+    {PAL, "720×576"},
+    {_720p, "1280×720"},
+    {WXGAHD, "1366×768"},
+    {_1080p, "1920×1080"},
+    {_2K, "2560×1440"},
+    {_4K, "3840×2160"},
+    {_5K, "5120×2880"},
+    {_8K, "7680×4320"},
+    {WXGA, "1280×800"},
+    {WXGAPRO, "1440×900"},
+    {WSXGAPRO, "1680×1050"},
+    {WUXGA, "1920×1200"},
+    {UWHD, "2560×1080"},
+    {UWQHD, "3440×1440"},
+    {WQHDPRO, "3840×1600"},
+    {iPhone5, "1136×640"},
+    {iPhone6, "1334×750"},
+    {iPhone6Plus, "1920×1080"},
+    {iPhoneX, "2436×1125"},
+    {iPhoneXS, "2688×1242"},
+    {iPhone121314, "1170×2532"},
+    {iPhone121314Pro, "1284×2778"}
+};
+
+static const QMap<ImageResolution, QSize> ImageResolutionSizeMap = {
+    {VGA, QSize(640, 480)},
+    {NTSC, QSize(720, 480)},
+    {PAL, QSize(720, 576)},
+    {_720p, QSize(1280, 720)},
+    {WXGAHD, QSize(1366, 768)},
+    {_1080p, QSize(1920, 1080)},
+    {_2K, QSize(2560, 1440)},
+    {_4K, QSize(3840, 2160)},
+    {_5K, QSize(5120, 2880)},
+    {_8K, QSize(7680, 4320)},
+    {WXGA, QSize(1280, 800)},
+    {WXGAPRO, QSize(1440, 900)},
+    {WSXGAPRO, QSize(1680, 1050)},
+    {WUXGA, QSize(1920, 1200)},
+    {UWHD, QSize(2560, 1080)},
+    {UWQHD, QSize(3440, 1440)},
+    {WQHDPRO, QSize(3840, 1600)},
+    {iPhone5, QSize(1136, 640)},
+    {iPhone6, QSize(1334, 750)},
+    {iPhone6Plus, QSize(1920, 1080)},
+    {iPhoneX, QSize(2436, 1125)},
+    {iPhoneXS, QSize(2688, 1242)},
+    {iPhone121314, QSize(1170, 2532)},
+    {iPhone121314Pro, QSize(1284, 2778)}
 };
 
 namespace imageconversion {
@@ -240,8 +294,8 @@ struct Data {
 
 struct Param {
     int fps;
-    int quality;
-    QString resolution;
+    ImageQuality quality;
+    ImageResolution resolution;
     int width;
     int height;
     bool loop;
