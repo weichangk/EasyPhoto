244c35c39ff87e1f1e090429f51c3e3c3251d4b2
weichangk <18276743761@163.com>
2024-07-24 08:00:54 +0800

    feat(upscayl)

--- imagekit/inc/imageupscaylsettingview.h
+++ imagekit/inc/imageupscaylsettingview.h
@@ -7,6 +7,9 @@
 
 #pragma once
 #include "../agui/inc/abasewidget.h"
+#include "../awidget/inc/alabel.h"
+#include "../awidget/inc/apushbutton.h"
+#include "../awidget/inc/aslider.h"
 
 namespace imageupscayl {
 class ImageUpscaylSettingView : public ABaseWidget {
@@ -19,5 +22,26 @@ protected:
     void createUi() override;
     void changeLanguage() override;
     void sigConnect() override;
+
+private:
+    ALabel *model_lab_ = nullptr;
+    APushButton *model_btn_ = nullptr;
+
+    ALabel *fmt_lab_ = nullptr;
+    APushButton *png_fmt_btn_ = nullptr;
+    APushButton *jpg_fmt_btn_ = nullptr;
+    APushButton *webp_fmt_btn_ = nullptr;
+
+    ALabel *scale_lab_ = nullptr;
+    ASlider *scale_slider_ = nullptr;
+
+    ALabel *compression_lab_ = nullptr;
+    ASlider *compression_slider_ = nullptr;
+
+    ALabel *output_folder_lab_ = nullptr;
+    APushButton *output_folder_btn_ = nullptr;
+
+    ALabel *reset_lab_ = nullptr;
+    APushButton *reset_btn_ = nullptr;
 };
 } // namespace imageupscayl
