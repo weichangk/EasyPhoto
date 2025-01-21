fb863e7948f70bf864064544022ddebe6b68ac22
weichangk <18276743761@163.com>
2025-01-22 00:57:54 +0800

    feat：绘制修改

--- src/conversion/listdelegate.cpp
+++ src/conversion/listdelegate.cpp
@@ -14,10 +14,7 @@ ListDelegate::ListDelegate(QObject *parent) :
 
 void ListDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const {
     auto data = index.data(Qt::UserRole).value<Data>();
-    painter->save();
     painter->setRenderHint(QPainter::Antialiasing, true);
-    painter->setPen(Qt::NoPen);
-    painter->setBrush(Qt::NoBrush);
 
     QRect rc = option.rect;
     bool pressed = m_EventType == QEvent::MouseButtonPress && rc.contains(m_CurPos);
@@ -25,42 +22,40 @@ void ListDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option, 
     bool hover = option.state & QStyle::State_MouseOver;
     bool selected_or_hover = selected || hover;
 
-    auto pixmapRect = rc.adjusted(0, 24, -8, -8);
-    Painter::paintPixmap(painter, pixmapRect, data.thumbnail, 1, 8, true);
-
-    auto borderRect = rc.adjusted(0, 0, -8, -8);
-    painter->setBrush(QColor("#2F2D2D"));
-    painter->drawRoundedRect(borderRect, 8, 8);
+    auto bgRect = rc.adjusted(0, 0, -8, -8);
+    
+    QColor bgColor = QColor("#232334");
+    if (hover) {
+        bgColor = QColor("#463c6c");
+    }
+    painter->setBrush(bgColor);
+    painter->drawRoundedRect(bgRect, 8, 8);
     painter->setBrush(Qt::NoBrush);
 
-    auto checkedconRect = QRect(borderRect.x() + 4, borderRect.y() + 4, data.checked_icon.width(), data.checked_icon.height());
-    Painter::paintPixmap(painter, checkedconRect, data.is_checked ? data.checked_icon : data.unchecked_icon, 1, 0, true);
+    auto pixmapRect = rc.adjusted(4, 24, -12, -12);
+    Painter::paintPixmap(painter, pixmapRect, data.thumbnail, 1, 8, true);
 
+    auto checkedRect = QRect(bgRect.x() + 4, bgRect.y() + 4, 16, 16);
+    Painter::paintPixmap(painter, checkedRect, data.is_checked ? data.checked_icon : data.unchecked_icon, 1, 0, true);
+
+    auto delRect = QRect(bgRect.right() - 4 - 16, bgRect.y() + 4, 16, 16);
     if (hover) {
-        auto delIconRect = QRect(borderRect.x() + borderRect.width() - data.delete_icon.width() - 4, borderRect.y() + 4, data.delete_icon.width(), data.delete_icon.height());
-        Painter::paintPixmap(painter, delIconRect, data.delete_icon, 1, 0, true);
+        Painter::paintPixmap(painter, delRect, data.delete_icon, 1, 0, true);
     }
 
-    QPen pen(QColor("#1F1F1F"));
-    pen.setWidth(1);
-    painter->setPen(pen);
-    if (hover || pressed) {
-        painter->drawRoundedRect(borderRect, 8, 8);
-    }
-    painter->setPen(Qt::NoPen);
-
-    pen.setColor(QColor("#575859"));
+    auto nameRect = QRect(checkedRect.right() + 4, checkedRect.y(), delRect.x() - checkedRect.right() - 4, 16);
+    QColor nameColor = QColor("#ffffff");
+    QPen pen(nameColor);
     painter->setPen(pen);
     QFont font = painter->font();
-    font.setPointSizeF(11);
+    font.setPointSizeF(10);
     painter->setFont(font);
     QString fileName = data.file_name;
-    auto nameRect = QRect(borderRect.x() + 20, borderRect.y(), borderRect.width() - 40, 24);
     QFontMetricsF metrics(font);
     if (metrics.horizontalAdvance(fileName) > nameRect.width()) {
-        fileName = metrics.elidedText(fileName, Qt::ElideMiddle, nameRect.width(), Qt::TextShowMnemonic);
+        fileName = metrics.elidedText(fileName, Qt::ElideMiddle, nameRect.width());
     }
-    painter->drawText(nameRect, Qt::PlainText, fileName);
+    painter->drawText(nameRect, Qt::AlignLeft | Qt::AlignVCenter, fileName);
     painter->setPen(Qt::NoPen);
 }
 
