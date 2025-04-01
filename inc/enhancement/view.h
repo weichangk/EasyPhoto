0cbb072860f9133a913ea52ba5ac546d091a691f
weichangk <18276743761@163.com>
2025-04-02 00:35:44 +0800

    feat:更新

--- inc/enhancement/view.h
+++ inc/enhancement/view.h
@@ -1,8 +1,28 @@
 #pragma once
 #include "globalview.h"
 #include "mvp/view.h"
+#include "widget/button.h"
+#include "widget/listview.h"
+#include "core/font.h"
+#include "core/object.h"
+#include "core/file.h"
+#include "filter/languagefilter.h"
+#include "filter/popupwindow.h"
+#include "filter/maskwidget.h"
+#include "filter/comboboxfilter.h"
+#include "importguide.h"
+
+#include <QStackedLayout>
+#include <QCheckBox>
+#include <QLabel>
+#include <QComboBox>
+#include <QLineEdit>
+#include <QPushButton>
 
 using namespace qtmaterialmvp;
+using namespace qtmaterialwidget;
+using namespace qtmaterialcore;
+using namespace qtmaterialfilter;
 
 class EnhancementView : public QWidget, public View {
     Q_OBJECT
@@ -14,4 +34,35 @@ public:
 private:
     void createUi();
     void connectSig();
+    void loadSampleImage();
+
+private Q_SLOTS:
+    void onLanguageChange();
+
+private:
+    LanguageFilter *m_pLanguageFilter = nullptr;
+
+    QLabel *m_pTitleLbl = nullptr;
+    QWidget *m_pBodyWidget = nullptr;
+
+    QWidget *m_pLeftWidget = nullptr;
+    QWidget *m_pRightWidget = nullptr;
+
+    QStackedLayout *m_pLeftWidgetStackedLayout = nullptr;
+
+    QWidget *m_pImportGuideWidget = nullptr;
+    QWidget *m_pSmapleWidget = nullptr;
+    QLabel *m_pSmapleTitleLbl = nullptr;
+    QLabel *m_pSmaple1ImageLbl = nullptr;
+    QLabel *m_pSmaple2ImageLbl = nullptr;
+    ImportGuide *m_pImportGuide = nullptr;
+
+    QLabel *m_pOutputFolderLbl = nullptr;
+    QComboBox *m_pOutputFolderCbb = nullptr;
+    ComboBoxFilter *m_pOutputFolderCbbFilter = nullptr;
+    IconButton *m_pOpenOutputFolderBtn = nullptr;
+
+    IconButton *m_pClearFileBtn = nullptr;
+    IconButton *m_pPreviewBtn = nullptr;
+    IconButton *m_pStartAllBtn = nullptr;
 };
--- src/enhancement/view.cpp
+++ src/enhancement/view.cpp
@@ -1,19 +1,109 @@
 #include "enhancement/view.h"
 
+#include <QCoreApplication>
+
 EnhancementView::EnhancementView(QWidget *parent) :
     QWidget(parent) {
     createUi();
     connectSig();
+    onLanguageChange();
 }
 
 void EnhancementView::createUi() {
     setObjectName("EnhancementView");
     setAttribute(Qt::WA_StyledBackground);
 
-    QLabel *label = new QLabel("EnhancementView", this);
+    m_pLanguageFilter = new LanguageFilter(this);
+
+    m_pTitleLbl = new QLabel(this);
+    m_pTitleLbl->setObjectName("EnhancementView_m_pTitleLbl");
+    auto titleLabLayout = new QHBoxLayout();
+    titleLabLayout->setContentsMargins(20, 0, 0, 0);
+    titleLabLayout->addWidget(m_pTitleLbl, 0, Qt::AlignLeft);
+
+    m_pBodyWidget = new QWidget(this);
+    auto bodyWidgetLayout = new QHBoxLayout(m_pBodyWidget);
+    bodyWidgetLayout->setContentsMargins(0, 0, 0, 0);
+    bodyWidgetLayout->setSpacing(0);
+
+    m_pLeftWidget = new QWidget(this);
+    auto LeftWidgetLayout = new QHBoxLayout(m_pLeftWidget);
+    LeftWidgetLayout->setContentsMargins(0, 0, 0, 0);
+    LeftWidgetLayout->setSpacing(0);
+
+    m_pRightWidget = new QWidget(this);
+    m_pRightWidget->setFixedWidth(320);
+    m_pRightWidget->setStyleSheet("background-color: blue;");
+
+    bodyWidgetLayout->addWidget(m_pLeftWidget, 1);
+    bodyWidgetLayout->addWidget(m_pRightWidget);
+
+    m_pLeftWidgetStackedLayout = new QStackedLayout();
+
+    m_pImportGuideWidget = new QWidget(this);
+    QVBoxLayout *importGuideLayout = new QVBoxLayout(m_pImportGuideWidget);
+    importGuideLayout->setContentsMargins(20, 20, 20, 20);
+    importGuideLayout->setSpacing(0);
+
+    m_pImportGuide = new ImportGuide(this);
+
+    m_pSmapleWidget = new QWidget(this);
+    m_pSmapleWidget->setFixedHeight(100);
+    auto smapleWidgetLayout = new QVBoxLayout(m_pSmapleWidget);
+    smapleWidgetLayout->setContentsMargins(0, 0, 0, 0);
+    smapleWidgetLayout->setSpacing(0);
+
+    m_pSmapleTitleLbl = new QLabel(this);
+    m_pSmapleTitleLbl->setAlignment(Qt::AlignCenter);
+
+    m_pSmaple1ImageLbl = new QLabel(this);
+    m_pSmaple1ImageLbl->setFixedSize(128, 72);
+    m_pSmaple2ImageLbl = new QLabel(this);
+    m_pSmaple2ImageLbl->setFixedSize(128, 72);
+
+    auto sampleImageLayout = new QHBoxLayout();
+    sampleImageLayout->setContentsMargins(0, 0, 0, 0);
+    sampleImageLayout->setSpacing(14);
+    sampleImageLayout->addStretch();
+    sampleImageLayout->addWidget(m_pSmaple1ImageLbl);
+    sampleImageLayout->addWidget(m_pSmaple2ImageLbl);
+    sampleImageLayout->addStretch();
+
+    smapleWidgetLayout->addWidget(m_pSmapleTitleLbl);
+    smapleWidgetLayout->addLayout(sampleImageLayout);
+
+    importGuideLayout->addWidget(m_pImportGuide, 1);
+    importGuideLayout->addSpacing(14);
+    importGuideLayout->addWidget(m_pSmapleWidget);
+    
+    m_pLeftWidgetStackedLayout->addWidget(m_pImportGuideWidget);
+
+    auto leftWidgetStackedMarginLayout = new QVBoxLayout();
+    leftWidgetStackedMarginLayout->setContentsMargins(0, 0, 0, 0);
+    leftWidgetStackedMarginLayout->addLayout(m_pLeftWidgetStackedLayout, 1);
+
+    LeftWidgetLayout->addLayout(leftWidgetStackedMarginLayout, 1);
+
     auto layout = new QVBoxLayout(this);
-    layout->addWidget(label, 0, Qt::AlignCenter);
+    layout->setContentsMargins(0, 0, 0, 0);
+    layout->setSpacing(0);
+    layout->addLayout(titleLabLayout, 0);
+    layout->addWidget(m_pBodyWidget, 1);
+
+    loadSampleImage();
 }
 
 void EnhancementView::connectSig() {
+    connect(m_pLanguageFilter, &LanguageFilter::sigLanguageChange, this, &EnhancementView::onLanguageChange);
+}
+
+void EnhancementView::loadSampleImage() {
+    QString execDir = QCoreApplication::applicationDirPath();
+    m_pSmaple1ImageLbl->setPixmap(QPixmap(QString("%1/sample1.webp").arg(execDir)).scaled(m_pSmaple1ImageLbl->size(), Qt::IgnoreAspectRatio, Qt::SmoothTransformation));
+    m_pSmaple2ImageLbl->setPixmap(QPixmap(QString("%1/sample2.webp").arg(execDir)).scaled(m_pSmaple2ImageLbl->size(), Qt::IgnoreAspectRatio, Qt::SmoothTransformation));
+}
+
+void EnhancementView::onLanguageChange() {
+    m_pTitleLbl->setText(tr("Ai Image Enhancer"));
+    m_pSmapleTitleLbl->setText(tr("Try with one of our smaples!"));
 }
